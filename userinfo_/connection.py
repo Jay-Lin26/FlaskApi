f3179950ff67f04098364637d0d3359cddfd198f
jay_Lin26 <z64666760@163.com>
2020-12-12 01:06:22 +0800

    密码加密，注册加入邮箱

---
+++ userinfo_/connection.py
@@ -0,0 +1,22 @@
+import pymysql
+
+
+def select(sentence):
+    try:
+        conn = pymysql.connect(
+            host='47.100.164.202',
+            user='root',
+            password='123456',
+            database='Testcases',
+        )
+        cursor = conn.cursor()
+        sql = sentence
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        conn.commit()
+        conn.close()
+        return result
+    except IndexError:
+        return {'message': ' select IndexError '}
+    except pymysql.err.OperationalError:
+        return {'message': ' select OperationalError'}
--- userinfo_/userinfo_api.py
+++ userinfo_/userinfo_api.py
@@ -1,30 +1,10 @@
 from flask import Flask, make_response, jsonify, request
+from userinfo_.Md5_salt import encryption
+from connection import select
 import pymysql
 
+
 app = Flask(__name__)
-
-"""初始化数据库"""
-
-
-def select(sentence):
-    try:
-        conn = pymysql.connect(
-            host='47.100.164.202',
-            user='root',
-            password='123456',
-            database='Testcases',
-        )
-        cursor = conn.cursor()
-        sql = sentence
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        conn.commit()
-        conn.close()
-        return result
-    except IndexError:
-        return jsonify({'message': ' select IndexError '})
-    except pymysql.err.OperationalError:
-        return jsonify({'message': ' select OperationalError'})
 
 
 @app.errorhandler(404)
@@ -37,7 +17,7 @@ def not_found(error):
     return make_response(jsonify({'error': 'Method Not Allowed'}), 405)
 
 
-"""获取用户信息"""
+"""获取用户列表"""
 
 
 @app.route('/userinfo/', methods=['GET'], strict_slashes=False)
@@ -64,6 +44,7 @@ def user_info():
                 'email': email
             }
             message.append(start_message)
+        # _message = json.dumps(message, sort_keys=True, indent=4, separators={',', ':'})
         return jsonify({'code': 200, 'data': message})
     except IndexError:
         return jsonify({'user_info': 'user_info index error', 'code': 200})
@@ -83,24 +64,25 @@ def user_login():
 
     username = request.form.get('username')
     password = request.form.get('password')
+    # 密码md5后验证
+    _password = encryption(password)
     """判断是否为空或空格"""
     if len(username) == 0 or username.isspace() == True or len(password) == 0 or password.isspace() == True:
         return jsonify({'message': '密码不能为空或空格', 'code': '0'})
 
-    sql = "select name from member"
-    select_username = select(sql)
+    name_sql = "select name from member"
+    pwd_sql = "select pwd from member where name = '%s'" % username
+    select_username = select(name_sql)
     list_name = []
     for i in range(len(select_username)):
         name = select_username[i][0]
         list_name.append(name)
-
-    sql = "select pwd from member where name = '%s'" % username
     try:
-        pwd = select(sql)[0][0]
+        pwd = select(pwd_sql)[0][0]
     except IndexError:
         return jsonify({'message': '用户名或密码错误', 'code': 200})
 
-    if password == pwd and username in list_name:
+    if _password == pwd and username in list_name:
         return jsonify({'message': 'success', 'code': 200})
     else:
         return jsonify({'message': '用户名或密码错误', 'code': 200})
@@ -113,13 +95,21 @@ def user_login():
 def user_register():
     username = request.form.get('username')
     password = request.form.get('password')
+    # 密码加密
+    _password = encryption(password)
+    email = request.form.get('email')
     """判断是否为空或空格"""
-    if len(username) == 0 or username.isspace() == True or len(password) == 0 or password.isspace() == True:
+    if len(username) == 0 or username.isspace() == True:
+        return jsonify({'message': '用户名不能为空或空格', 'code': '0'})
+    if len(password) == 0 or password.isspace() == True:
         return jsonify({'message': '密码不能为空或空格', 'code': '0'})
+    if len(email) == 0 or email.isspace == True:
+        return jsonify({'message': '邮箱不能为空或空格', 'code': '0'})
 
-    sql = "select name from member"
-    insert_sql = "insert into member (`name`, `pwd`) values ('%s', '%s')" % (username, password)
-    select_username = select(sql)
+    name_sql = "select name from member"
+    email_sql = "select email from member"
+    insert_sql = "insert into member (`name`, `pwd`, `email`) values ('%s', '%s', '%s')" % (username, _password, email)
+    select_username = select(name_sql)
     list_name = []
     for i in range(len(select_username)):
         name = select_username[i][0]
