8d72c69f626e440153415d17e56aa1f3acc96da0
jay_Lin26 <z64666760@163.com>
2021-01-06 22:44:38 +0800

    验证码入库

--- userinfo_/account_number.py
+++ userinfo_/account_number.py
@@ -1,6 +1,9 @@
 from email.mime.text import MIMEText
+from connection import select
 from random import randint
-import hashlib, smtplib
+import hashlib
+import smtplib
+import time, datetime
 
 """ 密码加密"""
 
@@ -9,7 +12,7 @@ def encryption(password):
     # 生成md5对象
     md5 = hashlib.md5(b'zhou')
     # 对数据加密
-    md5.update(password.encode('utf-8'))
+    md5.update(password.encode('utf8'))
     # 获取密文
     pwd = md5.hexdigest()
     return pwd
@@ -29,7 +32,7 @@ def Email_code():
 """ 发送邮件 """
 
 
-def send_email(rec_user):
+def send_email(user_email):
     # 第三方 smtp 服务
     mail_host = 'smtp.163.com'
     mail_user = 'z64666760@163.com'
@@ -37,21 +40,29 @@ def send_email(rec_user):
 
     sender = 'z64666760@163.com'
     # 需要发送的邮件内容
+    """ 随机验证码 """
     code = Email_code()
     content = MIMEText('您的验证码是：%s' % code)
     content['Subject'] = '登录验证码'     # 邮件主题
     content['From'] = sender        # 发件人
-    content['To'] = rec_user        # 收件人
+    content['To'] = user_email        # 收件人
     # 连接邮箱服务器；smtp端口是25
     try:
         smtp = smtplib.SMTP()
         smtp.connect(mail_host, port=25)
         smtp.login(mail_user, password)    # 登录邮箱
-        smtp.sendmail(sender, rec_user, content.as_string())
+        smtp.sendmail(sender, user_email, content.as_string())
         smtp.quit()
         return code
     except ConnectionRefusedError:
         return {'message': '由于目标计算机积极拒绝，无法连接', 'code': 10061}
     except smtplib.SMTPAuthenticationError:
         return {'message': 'user has no permission', 'code': 550}
+    # 插入数据到数据库中
+    # 获取当前时间
+    now_time = int(time.time())
+    sql = "insert into email_code (`email`, `e_code`, `date`) values ('%s', '%s', '%s')" % (user_email, code, now_time)
+    select(sql)
+
+
 
--- userinfo_/member.py
+++ userinfo_/member.py
@@ -1,10 +1,13 @@
 from flask import Flask, make_response, jsonify, request
-from userinfo_.account_number import encryption, Email_code, send_email
+from userinfo_.account_number import encryption, send_email
 from connection import select
+from flask_cors import CORS
 import pymysql
 
 
 app = Flask(__name__)
+# 解决跨域问题
+CORS(app)
 
 
 @app.errorhandler(404)
@@ -15,6 +18,15 @@ def not_found(error):
 @app.errorhandler(405)
 def not_found(error):
     return make_response(jsonify({'error': 'Method Not Allowed'}), 405)
+
+
+""" 发送验证码"""
+
+
+@app.route('/get_code/', methods=['GET'], strict_slashes=False)
+def send_email_():
+    user_email = request.args.get('email')
+    send_email(user_email)
 
 
 """获取用户列表"""
@@ -62,10 +74,9 @@ def user_login():
     request.form.get接收form_data参数fsf
     """
 
-    username = request.form.get('username')
+    username = request.form.get('username')  # 备注
     password = request.form.get('password')
-    email = request.form.get('email')
-    code = request.form.get('code')
+
     # 密码md5后验证
     _password = encryption(password)
     """判断是否为空或空格"""
@@ -73,15 +84,12 @@ def user_login():
         return jsonify({'message': '用户名不能为空或空格', 'code': '0'})
     if len(password) == 0 or password.isspace() == True:
         return jsonify({'message': '密码不能为空或空格', 'code': '0'})
-    if len(code) == 0 or code.isspace == True:
-        return jsonify({'message': '验证码不能为空', 'code': '0'})
 
     name_sql = "select name from member"
     pwd_sql = "select pwd from member where name = '%s'" % username
     select_username = select(name_sql)
     list_name = []
-    # 发送邮件
-    get_code = send_email(email)
+
     for i in range(len(select_username)):
         name = select_username[i][0]
         list_name.append(name)
@@ -90,14 +98,12 @@ def user_login():
     except IndexError:
         return jsonify({'message': '用户名或密码错误', 'code': 0})
 
-    if _password == pwd and username in list_name and code == get_code:
+    if _password == pwd and username in list_name:
         return jsonify({'message': 'success', 'code': 200})
     elif _password != pwd:
         return jsonify({'message': '密码错误', 'code': 0})
     elif username not in list_name:
         return jsonify({'message': '用户名错误', 'code': 0})
-    elif code != get_code:
-        return jsonify({'message': '验证码错误', 'code': 0})
 
 
 """注册"""
@@ -108,7 +114,7 @@ def user_register():
     username = request.form.get('username')
     password = request.form.get('password')
     email = request.form.get('email')
-    code = request.form.get('code')
+    # code = request.form.get('code')
     # 密码加密
     _password = encryption(password)
     """判断是否为空或空格"""
@@ -119,18 +125,27 @@ def user_register():
     if len(email) == 0 or email.isspace == True:
         return jsonify({'message': '邮箱不能为空或空格', 'code': '0'})
 
-    name_sql = "select name from member"
+    username_sql = "select name from member"
+    email_sql = "select email from member"
     insert_sql = "insert into member (`name`, `pwd`, `email`) values ('%s', '%s', '%s')" % (username, _password, email)
-    select_username = select(name_sql)
-    list_name = []
-    for i in range(len(select_username)):
-        name = select_username[i][0]
-        list_name.append(name)
+    username_result = select(username_sql)
+    email_result = select(email_sql)
+    username_list = []
+    email_list = []
+    for i in range(len(username_result)):
+        name = username_result[i][0]
+        username_list.append(name)
 
-    get_code = send_email(email)
-    if username in list_name:
+    for j in range(len(email_result)):
+        email_j = email_result[j][0]
+        email_list.append(email_j)
+
+    if username in username_list:
         return jsonify({'message': '用户名已存在', 'code': 0})
-    elif password not in list_name and code == get_code:
+    # elif email in email_list:
+    #     return jsonify({'message': '邮箱已存在', 'code': 0})
+    elif username not in username_list:
+        send_email(email)
         select(insert_sql)
         return jsonify({'message': '注册成功', 'code': 200})
 
