009fefe60c8cd3fad6a9ddf313122f0d07d2f0d1
jay-lin26 <z64666760@163.com>
2021-05-06 19:31:44 +0800

    2021/5/6 项目启动修改

--- manage.py
+++ manage.py
@@ -1,12 +1,19 @@
 # coding = utf-8
+import os
+import sys
+
 from flask import Flask
 from flask import make_response, jsonify, request
 from flask_cors import CORS
 
+from member.detail import detail_Blue
 from member.login import login_Blue
 from member.register import register_Blue
 from member.users_list import users_List_Blue
 from member.verification import verification_Blue
+
+current = os.getcwd()
+sys.path.append(current)
 
 app = Flask(__name__)
 # 设置启动配置
@@ -17,6 +24,7 @@ CORS(app)
 app.register_blueprint(login_Blue)  # 登录
 app.register_blueprint(register_Blue)
 app.register_blueprint(verification_Blue)
+app.register_blueprint(detail_Blue)  # 用户信息
 app.register_blueprint(users_List_Blue)
 
 
---
+++ member/__init__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+current_path = os.getcwd()
+sys.path.append(current_path)
+
+print(current_path)
