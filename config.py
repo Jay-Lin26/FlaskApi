8a1c5089b9fa00f8d4f7c53bcbb2327ce20df2ee
jay_Lin26 <z64666760@163.com>
2021-05-09 10:33:32 +0800

    2021/5/9 测试

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # coding = utf-8
 # 当前版本号
 VERSION = '1.0.1'
+#1
 # 项目依赖
 """
 Flask 1.1.2
@@ -8,10 +9,10 @@ Pymysql 0.10.0
 flask_CORS 3.0.10
 """
 # 数据库地址
-DB_HOST = '47.100.164.202'
+DB_HOST = '139.224.63.20'
 DB_USER = 'root'
-DB_PASSWORD = '123456'
-DB_DATABASE = 'Testcases'
+DB_PASSWORD = 'admin123'
+DB_DATABASE = 'flask_v1_pre'
 
 # Flask启动配置
 JSONIFY_MIMETYPE = 'application/json'
