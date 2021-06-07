e9c450f33dc6348a1ba89fb609c0119c5a35a7dc
jay-lin26 <z64666760@163.com>
2021-06-07 15:18:53 +0800

    2021/6/7 update

--- article/article_index.py
+++ article/article_index.py
@@ -1,13 +1,29 @@
 from flask.blueprints import Blueprint
 from common.db_utils import dbPerforms
 from flask import jsonify
+import time
 
 article_index_Blue = Blueprint("article_index_Blue", __name__)
 
 
 @article_index_Blue.route("/api/v1.0/article/index", methods=["get"])
 def article_index():
-    sql = "select id,title,description,image_url from article where status = '1' limit 8"
+    sql = """
+SELECT
+	a.id,
+	a.title,
+	a.description,
+	a.image_url,
+	m.`name`,
+	a.views,
+	a.release_time 
+FROM
+	article AS a
+	INNER JOIN member AS m ON a.mid = m.id 
+WHERE
+	`status` = 1 
+	LIMIT 8
+"""
     a = dbPerforms(sql)
     info = []
     for i in range(len(a)):
@@ -15,11 +31,19 @@ def article_index():
         t = a[i][1]
         d = a[i][2]
         u = a[i][3]
+        writer = a[i][4]
+        view = a[i][5]
+        times = a[i][6]
+        time_local = time.localtime(int(times))
+        pub_date = time.strftime("%Y-%m-%d", time_local)
         result = {
             'id': a_id,
             "title": t,
             "desc": d,
-            "img_url": u
+            "img_url": u,
+            "writer": writer,
+            "view": view,
+            "release_time": pub_date
         }
         info.append(result)
     return jsonify({"code": 200, "data": info})
