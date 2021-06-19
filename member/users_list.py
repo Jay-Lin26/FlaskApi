a448f15ceb29d28c473753bd34fe9a8c36590dfb
jay_Lin26 <z64666760@163.com>
2021-06-19 11:05:46 +0800

    2021/6/19 更新

--- member/users_list.py
+++ member/users_list.py
@@ -2,13 +2,11 @@
 from flask import Blueprint, jsonify
 
 from common.db_utils import dbPerforms
-from member.utils import loginRequired
 
 users_List_Blue = Blueprint('user', __name__)
 
 
 @users_List_Blue.route('/api/v1.0/member/list', methods=['GET'], strict_slashes=False)
-@loginRequired
 def usersList():
     __sql = "select * from member order by id limit 10"
     result = dbPerforms(__sql)
